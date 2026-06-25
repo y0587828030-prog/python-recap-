15edf2a2c6c576509aba5e683b46110899513eb9
yehoshua <y0587828030@gmail.com>
2026-06-25 09:47:50 +0300

    logical operatots

---
+++ recap.py
@@ -0,0 +1,24 @@
+
+#VARS:
+
+agent_name = "yehosh"
+mission_code =int(1234)
+distance = float(35.5)
+status_mission = True
+print(agent_name, mission_code, distance, status_mission)
+print(agent_name)
+print(mission_code)
+print(distance)
+print(status_mission)
+
+#LOGICAL OPERATORS:
+
+status = False
+print(not(status))
+
+age = 20
+id = True
+if age >= 18 and id == True:
+    print("enter")
+
+ 
